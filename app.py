488ac691a6c3d82cbca55a0471d6723a1429b924
Afifa Siddique <afeefaasiddiqueee98@gmail.com>
2025-04-05 20:59:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -62,6 +62,7 @@ def predict_intrusion(features):
 # --- Page 1: Welcome Section ---
 if st.session_state.page == 1:
     st.markdown("<h1 class='title'>🔍 Intrusion Detection System</h1>", unsafe_allow_html=True)
+    st.image('IMAGE.jpeg', use_container_width=True)
     st.markdown("<h2 class='description'>Protect Your Network from Unauthorized Access </h2>", unsafe_allow_html=True)
     st.markdown(
         """
