a00433d84237f28e3675123b464e8c178fb9144d
Afifa Siddique <afeefaasiddiqueee98@gmail.com>
2025-04-05 21:16:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def predict_intrusion(features):
 # --- Page 1: Welcome Section ---
 if st.session_state.page == 1:
     st.markdown("<h1 class='title'>🔍 CyberSentinel Guard</h1>", unsafe_allow_html=True)
-    st.image('IMAGE.jpg', use_container_width=False, width=1050)
+    st.image('IMAGE.jpg', use_container_width=False, width=2000)
 
     st.markdown("<h2 class='description'>Protect Your Network from Unauthorized Access </h2>", unsafe_allow_html=True)
     st.markdown(
